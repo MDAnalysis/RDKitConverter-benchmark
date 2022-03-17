6d7f5b51c4c3de83b0dca29f8a9cffe93acdc0d8
Cédric Bouysset <bouysset.cedric@gmail.com>
2022-03-17 04:29:17 +0100

    prepare molecules

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,16 @@
 from datetime import datetime
 from pathlib import Path
+from rdkit import Chem
+
+n_jobs = -2
+
 datapath = Path(__file__).resolve().parents[1] / "data"
 
 timestamp = datetime.now().strftime("%Y-%m-%d")
+
+delimiter = b"***\n"
+
+Chem.SetDefaultPickleProperties(
+    Chem.PropertyPickleOptions.MolProps | Chem.PropertyPickleOptions.PrivateProps)
+
+
