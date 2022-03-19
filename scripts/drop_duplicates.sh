2c5b1cf94a62177d2306740ca1199f226f399cde
Cédric Bouysset <bouysset.cedric@gmail.com>
2022-03-19 03:30:35 +0100

    remove duplicates on inchikey

---
+++ scripts/drop_duplicates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IN_FILE="chembl_processed.smi.gz"
+OUT_FILE="chembl_processed_unique.smi.gz"
+
+N_THREADS=$(grep -c ^processor /proc/cpuinfo)
+if [[ $N_WORKERS -lt 0 ]]; then
+    let N_THREADS+=1+$N_WORKERS
+else
+    N_THREADS=$N_WORKERS
+fi
+
+# go to data dir
+cd $(dirname "$0")/../data
+
+echo Removing duplicates
+# uniquify
+zcat "${IN_FILE}" \
+| sort -u -t' ' -k3,3 --parallel ${N_THREADS} \
+| cut -d' ' -f1,2 \
+| gzip > "${OUT_FILE}"
+
+# count
+zcat "${OUT_FILE}" | wc -l | tr -d '\n' > .processed_unique_count
+COUNT=$(cat .processed_unique_count)
+echo Wrote \'data/${OUT_FILE}\' with ${COUNT} unique entries
+
+cd - > /dev/null
