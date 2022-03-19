b0a531db2d6c3d1e0e92e875f06a55a70dcd6f08
Cédric Bouysset <bouysset.cedric@gmail.com>
2022-03-19 03:27:20 +0100

    revert to RunReactants

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,18 +1,44 @@
+import os
 from datetime import datetime
 from pathlib import Path
+from multiprocessing import cpu_count
 from rdkit import Chem
-from MDAnalysis.converters.RDKit import _infer_bo_and_charges, _standardize_patterns
+from MDAnalysis.converters.RDKit import (_infer_bo_and_charges,
+                                         _standardize_patterns)
 
-n_jobs = -2
 
-datapath = Path(__file__).resolve().parents[1] / "data"
+N_WORKERS = int(os.getenv("N_WORKERS", -1))
+if N_WORKERS < 0:
+    N_WORKERS = cpu_count() + 1 - N_WORKERS
+
+ROOT = Path(__file__).resolve().parents[1]
+DATA = ROOT / "data"
+RESULTS = ROOT / "results"
 
 timestamp = datetime.now().strftime("%Y-%m-%d")
 
-delimiter = b"***\n"
 
-Chem.SetDefaultPickleProperties(
-    Chem.PropertyPickleOptions.MolProps | Chem.PropertyPickleOptions.PrivateProps)
+def apply_reaction(rxn, mol):
+    for _ in range(mol.GetNumAtoms()):
+        mol.UpdatePropertyCache(strict=False)
+        Chem.Kekulize(mol)
+        products = rxn.RunReactants((mol,))
+        if products:
+            # structure: tuple[tuple[mol]]
+            # length of 1st tuple: number of matches in reactant
+            # length of 2nd tuple: number of products yielded by the reaction
+            # if there's no match in reactant, returns an empty tuple
+            # here we have at least one match, and the reaction used yield
+            # a single product hence `products[0][0]`
+            product = products[0][0]
+            # apply the next reaction to the product
+            mol = product
+        # exit the loop if there was nothing to transform
+        else:
+            break
+    mol.UpdatePropertyCache(strict=False)
+    Chem.Kekulize(mol)
+    return mol
 
 
 def add_Hs_remove_bo_and_charges(mol):
@@ -31,7 +57,6 @@ def add_Hs_remove_bo_and_charges(mol):
 
 def enumerate_reordered_mol(mol):
     """Enumerates all possible starting atoms for a given molecule"""
-    # go through each possible starting atom
     for root_atom in mol.GetAtoms():
         smi = Chem.MolToSmiles(mol, rootedAtAtom=root_atom.GetIdx())
         reordered_mol = Chem.MolFromSmiles(smi, sanitize=False)
@@ -49,17 +74,10 @@ def assign_bond_orders_and_charges(mol):
     return mol
 
 
-def is_isomorphic(mol, ref, useChirality=False):
-    return (mol.HasSubstructMatch(ref, useChirality=useChirality)
-            and ref.HasSubstructMatch(mol, useChirality=useChirality))
-
-
-def is_isomorphic_or_resonance_structure(mol, ref):
-    """Checks if 2 molecules are equal. If not, enumerates the resonance
-    structures of the first molecule and checks again"""
-    isomorphic = is_isomorphic(mol, ref)
-    if not is_isomorphic:
-        # try resonance structures
-        isomorphic = any(is_isomorphic(res_mol, ref)
-                         for res_mol in Chem.ResonanceMolSupplier(mol))
-    return isomorphic
+def same_molecules(mol, ref):
+    """Checks if 2 molecules are the same, using their resonance structures"""
+    same = mol.HasSubstructMatch(ref)
+    if not same:
+        same = bool(Chem.ResonanceMolSupplier(mol, maxStructs=50)
+                    .GetSubstructMatch(ref))
+    return same
